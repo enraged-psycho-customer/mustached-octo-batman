40daed1ab180296887aa739bd9fb7ab56914fe74
Pavel Kiselev <kpashka@gmail.com>
2013-07-07 23:40:47 +0600

    share link fix

--- protected/views/items/_social.php
+++ protected/views/items/_social.php
@@ -30,10 +30,10 @@
             ?>
         <?php endif; ?>
     </span>
-    <a href="https://vk.com/share.php?url=<?php echo Yii::app()->createAbsoluteUrl('/' . $model->id); ?>&text=Адовые клиенты!" title="Поделиться на VK" target="_blank">
+    <a href="https://vk.com/share.php?url=<?php echo Yii::app()->createAbsoluteUrl('/' . $model->id); ?>&title=Адовые клиенты!" title="Поделиться на VK" target="_blank">
         <i class="icon icon-vk"></i>
     </a>
-    <a href="https://twitter.com/share/?url=<?php echo Yii::app()->createAbsoluteUrl('/' . $model->id); ?>&title=Адовые клиенты!" title="Поделиться на Twitter" target="_blank">
+    <a href="https://twitter.com/share/?url=<?php echo Yii::app()->createAbsoluteUrl('/' . $model->id); ?>&text=Адовые клиенты!" title="Поделиться на Twitter" target="_blank">
         <i class="icon icon-twitter"></i>
     </a>
     <a href="javascript:void(0)" title="Поделиться на Facebook">

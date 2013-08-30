b3354ebc99ddb751c970e5d435f0893a45a755ec
Pavel Kiselev <kpashka@gmail.com>
2013-08-30 17:20:35 +0700

    dont count comments on update

--- protected/models/Comments.php
+++ protected/models/Comments.php
@@ -95,10 +95,12 @@ class Comments extends CActiveRecord
         parent::afterSave();
 
         /* @var $item Items */
-        $item = Items::model()->findByPk($this->item_id);
-        $item->comments_count = $item->comments_count + 1;
-        $item->updated_at = new CDbExpression('NOW()');
-        $item->save(false);
+        if ($this->isNewRecord) {
+            $item = Items::model()->findByPk($this->item_id);
+            $item->comments_count = $item->comments_count + 1;
+            $item->updated_at = new CDbExpression('NOW()');
+            $item->save(false);
+        }
     }
 
     /**

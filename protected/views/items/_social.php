2e4e3f348c23ea04f0676d9271f6cb656085a90e
Pavel Kiselev <kpashka@gmail.com>
2013-09-23 04:30:23 +0700

    updates

--- protected/views/items/_social.php
+++
@@ -1,5 +0,0 @@
-<div class="<?php echo $class; ?> social-common">
-    <a class="close" href="<?php if (isset($closeUrl)) echo $closeUrl; else echo 'javascript:void(0)'; ?> " title="Скрыть">
-        <i class="icon icon-close"></i>
-    </a>
-</div>

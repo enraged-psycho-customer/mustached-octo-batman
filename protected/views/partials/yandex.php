035ae770a9f5743cafeb5fc64aa3ff39ed5a4fd6
Pavel Kiselev <kpashka@gmail.com>
2013-09-30 02:34:15 +0700

    sharing stuff

--- protected/views/partials/yandex.php
+++ protected/views/partials/yandex.php
@@ -9,7 +9,7 @@
         'element' => 'ya_share',
         'link' => $this->createAbsoluteUrl('/' . $model->id),
         'title' => $title,
-        'description' => $description,
+        'description' => json_encode($description),
         'elementStyle' => array(
             'quickServices' => array(
                 'vkontakte',

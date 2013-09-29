ed5672a36ec07391d3698e21f2b23e738e6b59ac
Pavel Kiselev <kpashka@gmail.com>
2013-09-30 02:42:24 +0700

    sharing stuff

--- protected/views/partials/yandex.php
+++ protected/views/partials/yandex.php
@@ -1,12 +1,16 @@
 <div class="yandex">
     <?php
+    $title_parts = array(Yii::app()->name);
+    $category = Stages::getCategory($this->category);
+    if (!is_null($category)) $title_parts[] = $category;
+    $title = implode(" - ", $title_parts);
+
     $link = $this->createAbsoluteUrl('/' . $model->id);
 
     $config = array(
         'element' => 'ya_share',
-        'link' => $this->createAbsoluteUrl('/' . $model->id),
-        //'title' => $title,
-        //'description' => 'testets',
+        'link' => $link,
+        'title' => $title,
         'elementStyle' => array(
             'quickServices' => array(
                 'vkontakte',
@@ -18,16 +22,14 @@
     );
 
     if (isset($thumbnail)) $config['image'] = $this->createAbsoluteUrl($thumbnail);
+    if (isset($description)) $config['description'] = $description;
     $json_config = json_encode((object)$config);
     ?>
 
 
     <script type="text/javascript">
-        //var YaShareInstance = new Ya.share(<?php echo $json_config; ?>);
+        var YaShareInstance = new Ya.share(<?php echo $json_config; ?>);
     </script>
     <div id="ya_share">
-        <a href="http://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($link); ?>">
-            <i class="icon icon-facebook"></i>
-        </a>
     </div>
 </div>

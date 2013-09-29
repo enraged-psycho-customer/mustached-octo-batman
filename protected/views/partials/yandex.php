4869d70cced16481c15519bb6c95d762095d43d0
Pavel Kiselev <kpashka@gmail.com>
2013-09-30 03:06:37 +0700

    sharing stuff

--- protected/views/partials/yandex.php
+++ protected/views/partials/yandex.php
@@ -5,12 +5,18 @@
     if (!is_null($category)) $title_parts[] = $category;
     $title = implode(" - ", $title_parts);
 
+    $description = $this->viewLink;
+    if ($model->category != Items::CATEGORY_IMAGES) {
+        $description = mb_substr($this->description, 0, 100, 'utf-8') . "...";
+    }
+
     $link = $this->createAbsoluteUrl('/' . $model->id);
 
     $config = array(
         'element' => 'ya_share',
         'link' => $link,
         'title' => $title,
+        'description' => $description,
         'elementStyle' => array(
             'quickServices' => array(
                 'vkontakte',
@@ -21,16 +27,12 @@
         ),
         'serviceSpecific' => array(
             'twitter' => array(
-                'title' => isset($description) ? $description : '',
+                'title' => $description,
             )
         ),
     );
 
     if (isset($thumbnail)) $config['image'] = $this->createAbsoluteUrl($thumbnail);
-    $config['description'] = $this->viewLink;
-    if ($model->category != Items::CATEGORY_IMAGES) {
-        $config['description'] = mb_substr($this->description, 0, 100, 'utf-8') . "...";
-    }
     $json_config = json_encode((object)$config);
     ?>
 

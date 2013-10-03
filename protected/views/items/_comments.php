bdce8a3904d4ef4e2442d88725e44555e1fe58b6
Pavel Kiselev <kpashka@gmail.com>
2013-10-04 06:51:35 +0700

    comment fix

--- protected/views/items/_comments.php
+++ protected/views/items/_comments.php
@@ -3,7 +3,7 @@
 /* @var $comment Comments */
 /* @var $model Items */
 ?>
-
+<div class="comment"></div>
 <?php foreach ($comments[$index] as $comment): ?>
     <?php $this->renderPartial('_comment', array('index' => $index, 'comment' => $comment, 'model' => $model)); ?>
     <?php if (isset($comments[$comment->id])) $this->renderPartial('_comments', array('comments' => $comments, 'index' => $comment->id, 'model' => $model)); ?>
